3814f715f04e7fae03e7fc0927ed84cb9085bb8a
Dhimasekaputraa <dhimasekaputraa@gmail.com>
2026-08-14 20:33:35 +0700

    feat: shelf feature

--- app/src/main/java/com/dhimsea/dbook/domain/repository/BookRepository.kt
+++ app/src/main/java/com/dhimsea/dbook/domain/repository/BookRepository.kt
@@ -2,6 +2,8 @@ package com.dhimsea.dbook.domain.repository
 
 import com.dhimsea.dbook.domain.model.Annotation
 import com.dhimsea.dbook.domain.model.Book
+import com.dhimsea.dbook.domain.model.Shelf
+import com.dhimsea.dbook.domain.model.ShelfWithBooks
 import kotlinx.coroutines.flow.Flow
 
 interface BookRepository {
@@ -21,4 +23,11 @@ interface BookRepository {
     fun getAnnotationsForBook(bookId: Long): Flow<List<Annotation>>
     suspend fun insertAnnotation(annotation: Annotation): Long
     suspend fun deleteAnnotation(annotation: Annotation)
+
+    fun getAllShelvesWithBooks(): Flow<List<ShelfWithBooks>>
+    suspend fun createShelfWithBooks(shelfName: String, selectedBookIds: List<Long>): Boolean
+    suspend fun deleteShelf(shelfId: Long)
+    suspend fun removeBookFromShelf(shelfId: Long, bookId: Long)
+    suspend fun addBookToShelves(bookId: Long, shelfIds: List<Long>)
+    suspend fun updateShelfWithBooks(shelfId: Long, newName: String, orderedBookIds: List<Long>): Boolean
 }
